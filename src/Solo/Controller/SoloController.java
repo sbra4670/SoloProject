8cca63c405cb032f70dee360fe4088124f8eb07f
Sebastian <31518719+sbra4670@users.noreply.github.com>
2017-09-07 13:05:54 -0600

    made text display in console!

--- src/Solo/Controller/SoloController.java
+++ src/Solo/Controller/SoloController.java
@@ -3,4 +3,9 @@ package Solo.Controller;
 public class SoloController
 {
 
+	public void start ()
+	{
+System.out.println("Yay! I made my first Java project, solo!");
+	}
 }
+
--- src/Solo/Controller/SoloRunner.java
+++ src/Solo/Controller/SoloRunner.java
@@ -2,5 +2,9 @@ package Solo.Controller;
 
 public class SoloRunner
 {
-
+	public static void main (String [ ] args)
+	{
+		SoloController appSoloController = new SoloController( );
+		appSoloController.start();
+	}
 }
